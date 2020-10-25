42a4be53e370dd39711bc6a1b4d077498a858f56
Martina Hytychová <martina.hyt@gmail.com>
2020-10-26 00:01:36 +0100

    fix: no empty messages, disabled button

--- index.js
+++ index.js
@@ -4,19 +4,8 @@
 
 const messagesElement = document.querySelector('#messages');
 
-/*onst testMessages = [
-  { name: 'Pavel', message: 'Ahoj 👋', date: '11. 5. 2020 17:30:00' },
-  {
-    name: 'Martina',
-    message: 'Ja se máte?',
-    date: '11. 5. 2020 17:29:54',
-  },
-  { name: 'Michal', message: 'Nazdar', date: '12. 5. 2020 12:17:21' },
-  { name: 'Ivana', message: 'Ahoj', date: '12. 5. 2020 11:02:15' },
-];*/
-
+// komponenta vracející řetězec v podobě HTML zprávy
 const Message = (props) => {
-  // komponenta vracející řetězec v podobě HTML zprávy
   return `
   <div class="card mt-3 mb-3">
     <div class="card-body">
@@ -29,8 +18,8 @@ const Message = (props) => {
   </div>`;
 };
 
+// komponenta vracející jeden řetězec obsahující více zpráv
 const MessageList = (props) => {
-  // komponenta vracející jeden řetězec obsahující více zpráv
   let messagesData = '';
   for (let i = 0; i < props.items.length; i++) {
     messagesData += Message(props.items[i]);
@@ -45,15 +34,18 @@ const show = (messages) => {
   });
 };
 
+// funkce stahující zprávy ze serveru a přidávající je na stránku pomocí funkce show
 const fetchMessagesAndShow = () => {
-  // funkce stahující zprávy ze serveru a přidávající je na stránku pomocí funkce show
   fetch('https://czechichat.herokuapp.com/api/list-messages')
     .then((response) => response.json())
     .then((data) => show(data.messages));
 };
 
-setInterval(fetchMessagesAndShow, 2000); // Každé dvě sekundy zavolá updateMessages
-fetchMessagesAndShow(); // Stáhne a zobrazí nové zprávy hned po načtení stránky
+// Každé dvě sekundy zavolá updateMessages
+setInterval(fetchMessagesAndShow, 2000);
+
+// Stáhne a zobrazí nové zprávy hned po načtení stránky
+fetchMessagesAndShow();
 
 /* ========== ODESÍLÁNÍ ZPRÁV ========== */
 
@@ -62,7 +54,8 @@ const messageInputElement = document.querySelector('#message-input');
 const buttonElement = document.querySelector('#submit-button');
 
 const onSubmit = (event) => {
-  event.preventDefault(); // Zamezí přesměrování na jinou stránku při odesílání formuláře
+  // Zamezí přesměrování na jinou stránku při odesílání formuláře
+  event.preventDefault();
 
   const data = JSON.stringify({
     name: nameInputElement.value,
@@ -70,24 +63,36 @@ const onSubmit = (event) => {
   });
 
   //console.log('Data:', data);
-  buttonElement.disabled = true;
+
+  if (nameInputElement.value === '' || messageInputElement.value === '') {
+    return;
+  }
 
   // odešli data na server
-  if (nameInputElement.value !== '' || messageInputElement.value !== '') {
-    fetch('https://czechichat.herokuapp.com/api/send-message', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: data,
-    }).then((response) => {
-      console.log(response);
+  buttonElement.disabled = true;
+  fetch('https://czechichat.herokuapp.com/api/send-message', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: data,
+  })
+    .then((response) => {
+      /*
+    Server teoreticky mohl vrátit nějakou chybu, že zprávu neuložil. Ale v dokumentaci není napsané,jak chybu poznáme,
+    tak budeme serveru věřit, že když odpovídá, zprávu vždy přijme.
+    */
+      messageInputElement.value = '';
+      fetchMessagesAndShow();
+    })
+    .catch(() => {
+      alert(
+        'Je mi líto, ale server neodpovídá. Zkus zprávu odeslat za chvíli znovu.',
+      );
+    })
+    .finally(() => {
+      buttonElement.disabled = false;
     });
-  }
-  nameInputElement.value = '';
-  messageInputElement.value = '';
-
-  buttonElement.disabled = false;
 };
 
 document.querySelector('#send-form').addEventListener('submit', onSubmit);
